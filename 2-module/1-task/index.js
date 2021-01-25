1cc84162b5a4ce190aef81fa53746a7b3e9b28db
Iurii Mokrozub <quzelko@gmail.com>
2021-01-25 21:06:36 +0100

    2-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,14 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+
 function sumSalary(salaries) {
-  // ваш код...
+  let totalsalaries = 0;
+  for(let key in salaries) {
+
+    if(typeof salaries[key] === 'number' && isFinite(salaries[key])){  
+      totalsalaries += salaries[key];      
+    }
+  }   
+   return totalsalaries;    
 }
