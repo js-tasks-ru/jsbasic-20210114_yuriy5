e4b3b39f04b0fb07361b40c4cd444a5ae159e737
Iurii Mokrozub <quzelko@gmail.com>
2021-01-30 01:47:43 +0100

    3-module-3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,6 +2,36 @@
  * @param {string} str
  * @returns {string}
  */
+
+
+
+//
+// Does it make sense to use arrays to solve current task? It is string and we can resolve this task without unnecessary conversation to arrays and back.
+//
+
 function camelize(str) {
-  // ваш код...
+  if (typeof str !== 'string') return "String for modification wasn't defined";
+  
+  let convertedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    str[i] === "-" ? convertedStr += str[++i].toUpperCase()  : convertedStr += str[i];
+ }
+  
+  return convertedStr;
+}
+
+
+
+// Second version
+
+function camelize1(str) {
+  str = str.split('-');
+  str = str.map(
+      function(word, index) { 
+          return index == 0 ? word : word[0].toUpperCase() + word.slice(1);
+        
+        }
+    )
+
+  return str.join('');
 }
