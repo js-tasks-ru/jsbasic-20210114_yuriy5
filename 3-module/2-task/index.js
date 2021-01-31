2cfc9dc471c6c90b837a031ef053fd56ae09b8f1
Iurii Mokrozub <quzelko@gmail.com>
2021-01-31 18:26:30 +0100

    3-module-2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,15 @@
  * @param {number} b
  * @returns {number[]}
  */
+// first version
 function filterRange(arr, a, b) {
-  // ваш код...
+  let arr1 =[];
+  arr.forEach(item => item >= a && item <= b && arr1.push(item));
+
+  return arr1;  
+}
+
+// second version
+function filterRange1(arr, a, b) {
+  return arr.filter((item) => (a <= item && item <= b));
 }
