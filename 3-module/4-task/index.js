1e7ba35adfd620cc1ad19c6eefd9a8482b579ab6
Iurii Mokrozub <quzelko@gmail.com>
2021-02-01 02:24:54 +0100

    3-module-4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -4,8 +4,7 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(users, age) {
-  let result="";
+function showSalary(users, age, result = "") {
   users.forEach((item) => (item.age <= age) && (result +=`\n${item.name}, ${item.balance}`));
   return result.slice(1); 
 }
