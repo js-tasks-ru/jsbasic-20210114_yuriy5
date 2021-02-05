eca960f4d9dbdc122364fe16d5e1b01e1d3d62ac
Iurii Mokrozub <quzelko@gmail.com>
2021-02-05 01:47:05 +0100

    4-module-1-task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,9 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    for (let i = 0; i < table.rows.length; i++) {
+        table.rows[i].children[i].style.backgroundColor = 'red';
+    }
 }
+
+let table = document.body.firstElementChild;
